5c8b41906b1f9edbbc86554d78761e4fc065d894
Miro <miropaxjones@gmail.com>
2024-07-08 12:48:56 +1000

    Update copyright-year.js

--- copyright-year.js
+++ copyright-year.js
@@ -1,1 +1,1 @@
-
+ const targetDate = new Date("2025-01-01"); function displayYear() { const currentDate = new Date(); if (currentDate >= targetDate) { const year = currentDate.getFullYear(); document.getElementById("year").textContent = "-" + year; } } window.addEventListener("load", displayYear);
