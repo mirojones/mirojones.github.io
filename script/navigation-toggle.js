d59c3df0bd82436fe59486d66ea1b8a4b78b4aa1
Miro <miropaxjones@gmail.com>
2024-07-09 18:33:11 +1000

    Create navigation-toggle.js

---
+++ script/navigation-toggle.js
@@ -0,0 +1,8 @@
+function navigationToggle() {
+  var x = document.getElementById("navigation");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
