2476e908cc2a80723d4eda24b176f2dd4ed8ec29
Miro <miropaxjones@gmail.com>
2024-07-16 09:46:39 +1000

    Update settings.js

--- script/settings.js
+++ script/settings.js
@@ -16,6 +16,47 @@ function font(family, size) {
   document.body.style.fontSize = size;
 }
 
-window.addEventListener("load", (event) => {
-  new cursoreffects.bubblesCursor();
-});
+/*!
+ * Bubble Cursor.js
+ * - 90's cursors collection
+ * -- https://github.com/tholman/90s-cursor-effects
+ * -- https://codepen.io/tholman/full/qbxxxq/
+ */
+
+(function bubblesCursor() {
+  
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var cursor = {x: width/2, y: width/2};
+  var particles = [];
+  
+  function init() {
+    bindEvents();
+    loop();
+  }
+  
+  // Bind events that are needed
+  function bindEvents() {
+    document.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('resize', onWindowResize);
+  }
+  
+  function onWindowResize(e) {
+    width = window.innerWidth;
+    height = window.innerHeight;
+  }
+  
+  function onTouchMove(e) {
+    if( e.touches.length > 0 ) {
+      for( var i = 0; i < e.touches.length; i++ ) {
+        addParticle(e.touches[i].clientX, e.touches[i].clientY);
+      }
+    }
+  }
+  
+  function onMouseMove(e) {    
+    cursor.x = e.clientX;
+    cursor.y = e.clientY;
+    
+    addParticle( cursor.x, cursor.y);
+  }
