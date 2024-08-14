0ef64c737c57d03671ed1a7d56b4285fa4b5203e
Miro <miropaxjones@gmail.com>
2024-08-14 11:52:28 -0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -5,3 +5,14 @@ function bgColor(bg) {
 function textColor(txt) {
   document.body.style.color = txt;
 }
+
+function toggle(el) {
+    var div = document.getElementById(el);
+    
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
