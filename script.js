6235379f6a590626e0d178f7d71f46bcbd7a8740
Miro <miropaxjones@gmail.com>
2024-08-17 09:05:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function play() {
-  var audio = document.getElementById("kiribati");
-  audio.play();
+const yearsElements = document.getElementsByClassName("years");
+function checkYear() {
+  const currentYear = new Date().getFullYear();
+  if (currentYear >= 2025) {
+    for (let i = 0; i < yearsElements.length; i++) {
+      yearsElements[i].textContent = "2024-";
+    }
+  }
 }
+checkYear();
