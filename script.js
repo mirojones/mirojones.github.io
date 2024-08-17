4bc976d12dd88e80e651aec432b057706bb0f72f
Miro <miropaxjones@gmail.com>
2024-08-17 09:20:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const yearsElement = document.getElementById("years");
-var copyrightBeginYear = 2024;
-const currentYear = new Date().getFullYear();
-function checkYear() {
-  if (currentYear >= copyrightBeginYear + 1) {
-    yearsElement.innerHTML = copyrightBeginYear + "-" + currentYear;
-  } else
-    yearsElement.innerHTML = copyrightBeginYear;
-}
-checkYear();
+
